86f54351404147dd0676830f132297b3719066b2
EugeneHalimon <evgeniy@octaria.com>
2022-10-29 22:01:59 +0300

    GM-1: added auto addition of special symbols

--- src/components/PasswordGenerator/PasswordGenerator.tsx
+++ src/components/PasswordGenerator/PasswordGenerator.tsx
@@ -1,6 +1,12 @@
 const PasswordGenerator = () => {
     const numbers: number[] = Array(10).fill(0).map((num:number, i:number) => num + i)
-    console.log(numbers, 'NUMBERS')
+    const initSpecialSymbols: number[] = Array.from(Array(95)).map((_:undefined, i:number) => i + 33) 
+    const specialSymbols: string[] = initSpecialSymbols.map(code => String.fromCharCode(code))
+    .filter(char => char.match(/^[~`+!@#=$%^&*()_,.<>/?;:'"|/*-]*$/))
+    
+    
+    console.log("🚀 ~ file: PasswordGenerator.tsx ~ line 4 ~ PasswordGenerator ~ specialSymbols", specialSymbols)
+    
     return(
         <h1>TEST</h1>
     )
