7488507dd00363c45a1ab7ca2788354402c98e46
EugeneHalimon <evgeniy@octaria.com>
2022-10-29 21:11:36 +0300

    GM-1: fixed auto addition of numbers

--- src/components/PasswordGenerator/PasswordGenerator.tsx
+++ src/components/PasswordGenerator/PasswordGenerator.tsx
@@ -1,5 +1,5 @@
 const PasswordGenerator = () => {
-    const numbers = Array.from(Array(10)).fill(0).map((num:number, i:number) => num + i)
+    const numbers: number[] = Array(10).fill(0).map((num:number, i:number) => num + i)
     console.log(numbers, 'NUMBERS')
     return(
         <h1>TEST</h1>
