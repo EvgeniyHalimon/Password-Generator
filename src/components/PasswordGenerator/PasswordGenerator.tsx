2978099f6d2019a6de2f21be9d72ca94360595de
Женя <69395235+EvgeniyHalimon@users.noreply.github.com>
2022-10-30 19:52:27 +0200

    Feature/gm 1 (#1)
    * GM-1: added auto addition of numbers
    
    * GM-1: fixed auto addition of numbers
    
    * GM-1: added auto addition of special symbols
    
    * GM-1: added auto addition of english/cyrillic letters, added utils for gettinf letters
    
    * GM-1: refactored of PasswordGenerator and getCharacters func
    
    * GM-1: return for loop, added shuffle
    
    * GM-1: remove console, password display on the page
    
    * GM-1: remove useEffect
    
    Co-authored-by: EugeneHalimon <evgeniy@octaria.com>

---
+++ src/components/PasswordGenerator/PasswordGenerator.tsx
@@ -0,0 +1,53 @@
+import { getCharacters } from "../../utils/getCharacters"
+
+const PasswordGenerator = () => {
+    const numbers: number[] = Array(10).fill(0).map((num:number, i:number) => num + i)
+
+    const specialSymbols: string[] = getCharacters(95, 33)
+    .filter(char => char.match(/^[~`+!@#=$%^&*()_,.<>/?;:'"|/*-]*$/))
+
+    const engLowerCaseLetters: string[] = getCharacters(26, 97)
+
+    const engUpperCaseLetters: string[] = getCharacters(26, 65)
+
+    const cyrillicLowerCaseLetters : string[] = getCharacters(32, 1040)
+
+    const cyrillicUpperCaseLetters : string[] = getCharacters(32, 1072)
+    
+    const generatePassword = (
+        length: number, 
+        hasNumbers: boolean, 
+        hasSymbols: boolean, 
+        hasEngLowerCase: boolean, 
+        hasEngUpperCase: boolean,
+        hasCyrillicLowerCase: boolean, 
+        hasCyrillicUpperCase: boolean,
+    ) : string => {
+
+        const availableCharacters = [
+            ...(hasNumbers ? numbers : []),
+            ...(hasSymbols ? specialSymbols : []),
+            ...(hasEngLowerCase ? engLowerCaseLetters : []),
+            ...(hasEngUpperCase ? engUpperCaseLetters : []),
+            ...(hasCyrillicLowerCase ? cyrillicLowerCaseLetters : []),
+            ...(hasCyrillicUpperCase ? cyrillicUpperCaseLetters : []),
+        ].sort(() => Math.random() - 0.5)
+
+        if(availableCharacters.length === 0) return 'You must choose at least 1 option'
+
+        let password = ''
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * availableCharacters.length)
+            password += availableCharacters[randomIndex]
+        }
+
+        return password
+    }
+    
+    return(
+        <h1>{generatePassword(77, true, true, true, true, true, true,)}</h1>
+    )
+}
+
+export {PasswordGenerator}
