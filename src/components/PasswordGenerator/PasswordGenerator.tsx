c49377b2724860ac453bda9a702ffc1f2ec82475
EugeneHalimon <evgeniy@octaria.com>
2022-10-30 18:40:04 +0200

    GM-1: refactored of PasswordGenerator and getCharacters func

--- src/components/PasswordGenerator/PasswordGenerator.tsx
+++ src/components/PasswordGenerator/PasswordGenerator.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { getCharacters } from "../../utils/getCharacters"
 
 const PasswordGenerator = () => {
@@ -10,10 +11,42 @@ const PasswordGenerator = () => {
 
     const engUpperCaseLetters: string[] = getCharacters(26, 65)
 
-    const cyrillicLowerCaseLetters : string[] = getCharacters(32, 1040)
+    const cyrillicUpperCaseLetters : string[] = getCharacters(32, 1040)
 
-    const cyrillicUpperCaseLetters : string[] = getCharacters(32, 1072)
+    const cyrillicLowerCaseLetters : string[] = getCharacters(32, 1072)
     
+    const generatePassword = (
+        length: number, 
+        hasNumbers: boolean, 
+        hasSymbols: boolean, 
+        hasEngLowerCase: boolean, 
+        hasEngUpperCase: boolean,
+        hasCyrillicLowerCase: boolean, 
+        hasCyrillicUpperCase: boolean,
+    ) : string => {
+
+        const availableCharacters = [
+            ...(hasNumbers ? numbers : []),
+            ...(hasSymbols ? specialSymbols : []),
+            ...(hasEngLowerCase ? engLowerCaseLetters : []),
+            ...(hasEngUpperCase ? engUpperCaseLetters : []),
+            ...(hasCyrillicLowerCase ? cyrillicLowerCaseLetters : []),
+            ...(hasCyrillicUpperCase ? cyrillicUpperCaseLetters : []),
+        ]
+
+        if(availableCharacters.length === 0) return 'You must choose at least 1 option'
+
+        const password: string = availableCharacters.reduce((accum: string) => {
+            const randomIndex = Math.floor(Math.random() * availableCharacters.length)
+            return accum + availableCharacters[randomIndex]
+        },'')
+
+        return password
+    }
+
+    useEffect(() => {
+        console.log(generatePassword(100, true, true, true, true, true, true))
+    },[])
     
     return(
         <h1>TEST</h1>
--- src/utils/getCharacters.ts
+++ src/utils/getCharacters.ts
@@ -1,5 +1,5 @@
 const getCharacters = (length: number, initNumber: number) : string[] => {
-    const charactersCodeArray: number[] = Array.from(Array(length)).map((_:undefined, i:number) => i + initNumber)
+    const charactersCodeArray: number[] = Array.from({length: length}, (_:undefined, i:number) => i + initNumber)
     return charactersCodeArray.map(code => String.fromCharCode(code))
 }
 
