1144ec0f88f607c500ce239aad3e6d5382f815f3
EvgeniyHalimon <halimonevgeniy@gmail.com>
2022-11-29 18:20:49 +0200

    GM-3: fixed styles, added mode button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import './App.scss';
+import { useState } from 'react';
+import { IconButton } from '@mui/material';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { Layout } from './components/Layout';
 import PasswordGenerator from './components/PasswordGenerator/PasswordGenerator';
 
+import './App.scss';
+
 function App() {
+
+  const [mode, setMode] = useState(true)
+
   return (
     <Layout>
-        <PasswordGenerator/>
+      <IconButton onClick={() => setMode(!mode)}>
+        {mode ? <LightModeIcon/> : <DarkModeIcon/>}
+      </IconButton>
+      <PasswordGenerator/>
     </Layout>
   );
 }
